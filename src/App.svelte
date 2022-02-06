7f20c474af6392eea4fef5b4fa42bb76518f7d5e
Mohamed Muhsin Kamil <muhsinwahab@gmail.com>
2022-02-06 20:42:23 +0530

    Initialise svelte

---
+++ src/App.svelte
@@ -0,0 +1,22 @@
+<script>
+	export let name;
+</script>
+
+<main>
+	<h1>
+		Hello {name}
+	</h1>
+</main>
+
+<style>
+	main {
+		padding: 0; 
+		display: flex;
+		justify-content: center;
+	}
+	h1 {
+		font-size: 4rem;
+		color: red;
+		font-weight: 200;
+	}
+</style>
